1c46260af8afd80c92d16fb4258e1d0823376417
Wade Tobin <wadetobin@wades-mbp.hitronhub.home>
2017-10-27 15:33:08 -0500

    Final version of Color Navigation

---
+++ ColorsWithNavigation/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  ColorsWithNavigation
+//
+//  Created by Wade Tobin on 10/27/17.
+//  Copyright © 2017 Wade Tobin. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
---
+++ ColorsWithNavigation/ColorDetailViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ColorDetailViewController.swift
+//  ColorsWithNavigation
+//
+//  Created by Wade Tobin on 10/27/17.
+//  Copyright © 2017 Wade Tobin. All rights reserved.
+//
+
+import UIKit
+
+class ColorDetailViewController: UIViewController {
+    
+    var color: Color?
+
+    @IBOutlet weak var colorNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        colorNameLabel.text = color?.name
+        self.view.backgroundColor = color?.uiColor
+        self.title = color?.name
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
